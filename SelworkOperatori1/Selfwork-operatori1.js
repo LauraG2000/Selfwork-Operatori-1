447c4563f1f6f523c078032268e55378257a6235
LauraG2000 <lauragag19@gmail.com>
2024-12-13 15:15:01 +0100

    completato Selfwork Operatori 1

---
+++ SelworkOperatori1/Selfwork-operatori1.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+let num4 = 40;
+let num5 = 50;
+
+let somma = num1 + num2 + num3 + num4 + num5;
+let media = somma / 5;
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
